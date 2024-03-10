94f0d33ee50129cc383924c8dd350143df531271
ajay joshi <ajay.joshi@renewbuy.com>
2024-03-10 13:55:03 +0530

    add api error and response handler utility

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"})) // to handle json data with a limit of 16kb i.e. max 16kb of json is allowed
+app.use(express.urlencoded({extended: true, limit: "16kb"})) // to encode url while parsing
+app.use(express.static("public")) // for static files
+app.use(cookieParser()) // to handle cookies
+
+export { app }
